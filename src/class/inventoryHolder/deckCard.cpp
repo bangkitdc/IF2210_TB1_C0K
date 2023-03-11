0d7af7c9e174e3f9fbac42185f9e99d5aefd5383
auliamey <auliaannandya@gmail.com>
2023-03-11 17:33:18 +0700

    add straight, straight flush, threes and fix boolean

--- src/class/inventoryHolder/deckCard.cpp
+++ src/class/inventoryHolder/deckCard.cpp
@@ -4,31 +4,31 @@
 
 DeckCard::DeckCard() : InventoryHolder ("DeckCard"){
     for(int i=1;i<=13;i++){
-            cards.push_back(Card(i,"Merah"));
+            cards.push_back(Card(i,"merah"));
     }    
     for(int i=1;i<=13;i++){
-            cards.push_back(Card(i,"Kuning"));
+            cards.push_back(Card(i,"kuning"));
     }    
     for(int i=1;i<=13;i++){
-            cards.push_back(Card(i,"Biru"));
+            cards.push_back(Card(i,"biru"));
     }    
     for(int i=1;i<=13;i++){
-            cards.push_back(Card(i,"Hijau"));
+            cards.push_back(Card(i,"hijau"));
     }    
 }
 
 DeckCard::~DeckCard(){}
 
-void DeckCard::displayDeckCard(){
-    for (auto elem : DeckCard::cards) {
-        elem.displayCard();
-    }
-}
+// void DeckCard::displayDeckCard(){
+//     for (auto elem : DeckCard::cards) {
+//         elem.displayCard();
+//     }
+// }
 
-void DeckCard::shuffleCard(){
-    auto rng = std::default_random_engine {};
-    std::shuffle(begin(cards), end(cards), rng);
-}
+// void DeckCard::shuffleCard(){
+//     auto rng = std::default_random_engine {};
+//     std::shuffle(begin(cards), end(cards), rng);
+// }
 
 vector<Card> DeckCard::getCards(){
     return cards;
