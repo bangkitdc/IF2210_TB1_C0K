53af446554a264ede0eb7863519889e899779235
zaydanA <16521131@std.stei.itb.ac.id>
2023-03-12 21:28:22 +0700

    resolved conflict cardExcept

--- src/class/exception/cardException.hpp
+++ src/class/exception/cardException.hpp
@@ -7,57 +7,33 @@ using namespace std;
 
 class PlayerFullException : public BaseException{
     public : 
-<<<<<<< HEAD
-    playerFullException(){}
-    const string what() const {
-        return "Player sudah memiliki kartu" ;
-=======
     PlayerFullException(){}
     const string what() const noexcept {
         return "Player sudah memiliki kartu";
->>>>>>> main
     }
 };
 
 class PlayerKosongException : public BaseException{
     public :
-<<<<<<< HEAD
-    playerKosongException(){}
-    const string what() const {
-        return "Player tidak memiliki kartu" ;
-=======
     PlayerKosongException(){}
     const string what() const noexcept {
         return "Player tidak memiliki kartu";
->>>>>>> main
     }
 };
 
 class TableKosongException : public BaseException {
     public :
-<<<<<<< HEAD
-    tableKosongException(){} 
-    const string what() const{
-        return "Tidak ada TableCard" ;
-=======
     TableKosongException(){} 
     const string what() const noexcept {
         return "Tidak ada TableCard";
->>>>>>> main
     }
 };
 
 class TablePenuhException : public BaseException {
     public : 
-<<<<<<< HEAD
-    tablePenuhException(){}
-    const string what() const{
-        return "Table Card sudah ada 5" ;
-=======
     TablePenuhException(){}
     const string what() const noexcept {
         return "Table Card sudah ada 5";
->>>>>>> main
     }
 };
 #endif
