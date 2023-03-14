336ac84308021f8de46e3908e15382dcb2d4ea44
bangkitdc <bangkitdc@gmail.com>
2023-03-14 19:55:43 +0700

    fix: fix cangkul and adding few additional functions

--- src/class/inventoryHolder/main.cpp
+++ src/class/inventoryHolder/main.cpp
@@ -1,15 +1,15 @@
-#include "Player.cpp"
-#include "C:\Users\ASUS\TB1OOP\IF2210_TB1_C0K\src\class\cardValue\card.cpp"
-#include "tableCard.cpp"
-#include "inventoryHolder.cpp"
-#include "deckCard.cpp"
-int main(){
-    DeckCard game1;
-    int n;
-    Player p1("Player 1",game1,n);
-    tableCard table1;
-    game1.shuffleCard();
-    game1.displayDeckCard();
+// #include "Player.cpp"
+// #include "C:\Users\ASUS\TB1OOP\IF2210_TB1_C0K\src\class\cardValue\card.cpp"
+// #include "tableCard.cpp"
+// #include "inventoryHolder.cpp"
+// #include "deckCard.cpp"
+// int main(){
+//     DeckCard game1;
+//     int n;
+//     Player p1("Player 1",game1,n);
+//     tableCard table1;
+//     game1.shuffleCard();
+//     game1.displayDeckCard();
     // Player p1;
     // tableCard table1;
     // p1+game1; //kartu p1 ditambah kartu dari deckCard
@@ -23,4 +23,4 @@ int main(){
 
 
 
-}
+// }
